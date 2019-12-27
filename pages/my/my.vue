7ccec117046e70cedd6034c2c7174e259ac54f19
yang1004 <8146659@qq.com>
2019-12-27 20:11:55 +0800

    all-view

--- pages/my/my.vue
+++ pages/my/my.vue
@@ -1,6 +1,71 @@
 <template>
 	<view>
+		<view class="background"></view>
+		<view class="top">
+			<view class="user">
+				<view class="img">
+					<image src="../../static/images/images.jpg" mode=""></image>
+				</view>
+				<view class="middle">
+					<view class="name">
+						<view class="username">.</view>
+						<image src="../../static/images/lipinka (50).png" mode=""></image>
+					</view>
+					<view class="phone">17320140486</view>
+				</view>
+				<view class="right">
+					<view class="btn">打款账户</view>
+				</view>
+			</view>
+			<view class="userMoney">
+				<view class="left">
+					<view class="name">账户余额</view>
+					<view class="num">0.00</view>
+				</view>
+				<view class="right">
+					<view class="withdraw-deposit">提现</view>
+					<view class="record">记录</view>
+				</view>
+			</view>
+		</view>
 		
+		<view class="content">
+			<view class="item">
+				<image src="../../static/images/lipinka (53).png" mode=""></image>
+				<view class="name">我的分销</view>
+			</view>
+			<view class="item">
+				<image src="../../static/images/lipinka (93).png" mode=""></image>
+				<view class="name">推广赚钱</view>
+			</view>
+			<view class="item">
+				<image src="../../static/images/lipinka (55).png" mode=""></image>
+				<view class="name">联系我们</view>
+			</view>
+			<view class="item">
+				<image src="../../static/images/lipinka (65).png" mode=""></image>
+				<view class="name">企业回收</view>
+			</view>
+			<view class="item">
+				<image src="../../static/images/lipinka (55).png" mode=""></image>
+				<view class="name">招商合作</view>
+			</view>
+			<view class="item">
+				<image src="../../static/images/lipinka (93).png" mode=""></image>
+				<view class="name">常见问题</view>
+			</view>
+			<view class="item">
+				<image src="../../static/images/lipinka (53).png" mode=""></image>
+				<view class="name">意见反馈</view>
+			</view>
+			<view class="item">
+				<image src="../../static/images/lipinka (65).png" mode=""></image>
+				<view class="name">设置</view>
+			</view>
+			<view class="item">
+				
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -18,5 +83,151 @@
 </script>
 
 <style>
-
+	
+	/* 底部九宫格样式 */
+	.content{
+		width: 90%;
+		margin-left: 5%;
+		margin-top: 30upx;
+		height: 600upx;
+		background-color: #fff;
+	}
+	.content .item{
+		width: 33.3%;
+		height: 200upx;
+		border-right: 1px solid #ccc;
+		border-bottom: 1px solid #ccc;
+		float: left;
+		box-sizing: border-box;
+		text-align: center;
+	}
+	.content .item:nth-child(3),.content .item:nth-child(6),.content .item:nth-child(9){
+		border-right: none;
+	}
+	.content .item:nth-child(7),.content .item:nth-child(8),.content .item:nth-child(9){
+		border-bottom: none;
+	}
+	.content .item image{
+		width: 80upx;
+		height: 80upx;
+		margin-top: 20upx;
+	}
+	
+	page{
+		background-color: #f2f2f2;
+	}
+	.background{
+		position: absolute;
+		top: 0;
+		left: 0;
+		z-index: -1;
+		width: 100%;
+		height: 200upx;
+		background-color: #6ac4c0;
+	}
+	.top{
+		width: 90%;
+		height: 360upx;
+		margin-left: 5%;
+		margin-top: 100upx;
+		border-radius: 20upx;
+		padding: 30upx;
+		box-sizing: border-box;
+		background-color: #fff;
+	}
+	.top .user{
+		width: 100%;
+		height: 200upx;
+		padding-top: 30upx;
+		box-sizing: border-box;
+		border-bottom: 1px solid #ccc;
+	}
+	.top .user .img{
+		width: 130upx;
+		height: 130upx;
+		border-radius: 50%;
+		margin-right: 20upx;
+		float: left;
+		display: inline-block;
+	}
+	.top .user .img image{
+		width: 100%;
+		height: 100%;
+		border-radius: 50%;
+	}
+	.top .middle{
+		height: 100%;
+		padding-top: 30upx;
+		box-sizing: border-box;
+		display: inline-block;
+	}
+	.top .middle .name .username{
+		display: inline-block;
+		margin-right: 25upx;
+	}
+	.top .middle .name image{
+		width: 30upx;
+		height: 30upx;
+		display: inline-block;
+	}
+	.top .right{
+		width: 160upx;
+		height: 100%;
+		float: right;
+		display: inline-block;
+	}
+	.top .right .btn{
+		width: 100%;
+		height: 50upx;
+		margin-top: 35upx;
+		line-height: 50upx;
+		text-align: center;
+		border: 1px solid #c8332d;
+		background-color: #fff;
+		color: #c8332d;
+		border-radius: 30upx;
+	}
+	.top .userMoney{
+		width: 100%;
+		height: 100upx;
+	}
+	.top .userMoney .left{
+		display: inline-block;
+		width: 50%;
+		height: 100%;
+		padding-top: 20upx;
+		box-sizing: border-box;
+	}
+	.top .userMoney .left .name{
+		font-size: 28upx;
+		color: #4b4b4b;
+		line-height: 40upx;
+	}
+	.top .userMoney .left .num{
+		font-size: 36upx;
+		color: #5b5b5b;
+		line-height: 40upx;
+	}
+	.top .userMoney .right{
+		display: inline-block;
+		width: 50%;
+		height: 100%;
+	}
+	.top .userMoney .right .withdraw-deposit{
+		display: inline-block;
+		width: 150upx;
+		height: 60upx;
+		line-height: 60upx;
+		text-align: center;
+		background-color: #6ac4c0;
+		margin-top: 20upx;
+		border-radius: 10upx;
+		color: #fff;
+	}
+	.top .userMoney .right .record{
+		display: inline-block;
+		float: right;
+		line-height: 100upx;
+		color: #6ac4c0;
+	}
 </style>
---
+++ pages/sellCard/sellCard.vue
@@ -0,0 +1,262 @@
+<template>
+	<view>
+		<view class="top">
+			<view class="card1" @click="card1Click">
+				<view class="name">卡类</view>
+				<view class="card1Name">
+					<view class="left">话费卡</view>
+					<image src="../../static/images/cards1.png" mode=""></image>
+				</view>
+				<view class="right-icon">
+					<image src="../../static/images/right_1.png" mode=""></image>
+				</view>
+			</view>
+			<view class="card1">
+				<view class="name">卡种</view>
+				<view class="card1Name">
+					<view class="left">中国移动</view>
+					<image src="../../static/images/cards1.png" mode=""></image>
+				</view>
+				<view class="right-icon">
+					<image src="../../static/images/right_1.png" mode=""></image>
+				</view>
+			</view>
+			<view class="card1">
+				<view class="name">面值</view>
+				<view class="card1Name">
+					<view class="left">请选择面值</view>
+					<image src="../../static/images/cards1.png" mode=""></image>
+				</view>
+				<view class="right-icon">
+					<image src="../../static/images/right_1.png" mode=""></image>
+				</view>
+			</view>
+			<view class="card1">
+				<view class="name">回收价</view>
+				<view class="card1Name">
+					<view class="left"></view>
+					
+				</view>
+			</view>
+		</view>
+		<view class="middle">
+			<view class="left">获取方式</view>
+			<view class="step">交易步骤</view>
+			<view class="right">常见问题</view>
+		</view>
+		<view class="bottom">
+			<view class="title">用户须知</view>
+		</view>
+		
+		<!-- 卡类弹窗 -->
+		<view class="card1-box" v-if="card1Show">
+			<view class="title">请选择卡类</view>
+			<image class="close" src="../../static/images/close.png" @click="card1Close" mode=""></image>
+			<view class="cards-box">
+				<view class="card-item">
+					<view class="name">话费卡</view>
+					<image src="../../static/images/cards1.png" mode=""></image>
+				</view>
+				<view class="card-item">
+					<view class="name">话费卡</view>
+					<image src="../../static/images/cards1.png" mode=""></image>
+				</view>
+				<view class="card-item">
+					<view class="name">话费卡</view>
+					<image src="../../static/images/cards1.png" mode=""></image>
+				</view>
+				<view class="card-item">
+					<view class="name">话费卡</view>
+					<image src="../../static/images/cards1.png" mode=""></image>
+				</view>
+				<view class="card-item">
+					<view class="name">话费卡</view>
+					<image src="../../static/images/cards1.png" mode=""></image>
+				</view>
+				<view class="card-item">
+					<view class="name">话费卡</view>
+					<image src="../../static/images/cards1.png" mode=""></image>
+				</view>
+				<view class="card-item">
+					<view class="name">话费卡</view>
+					<image src="../../static/images/cards1.png" mode=""></image>
+				</view>
+				<view class="card-item">
+					<view class="name">话费卡</view>
+					<image src="../../static/images/cards1.png" mode=""></image>
+				</view>
+			</view>
+		</view>
+		<view id="mask1" v-if="card1Show" @click="card1Close"></view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				card1Show:false
+			}
+		},
+		methods: {
+			card1Click(){//卡类点击出现选择框
+				this.card1Show = true
+			},
+			card1Close(){
+				this.card1Show = false
+			}
+		}
+	}
+</script>
+
+<style>
+	
+	.card1-box{
+		position: fixed;
+		bottom: 0;
+		left: 0;
+		width: 100%;
+		height: 700upx;
+		background-color: #fff;
+		z-index: 99;
+	}
+	.card1-box .title{
+		width: 100%;
+		height: 100upx;
+		text-align: center;
+		line-height: 100upx;
+		font-size: 34upx;
+	}
+	.card1-box .close{
+		width: 50upx;
+		height: 50upx;
+		position: absolute;
+		right: 25upx;
+		top: 25upx;
+	}
+	.card1-box .cards-box{
+		width: 100%;
+		height: 600upx;
+		overflow-y: scroll;
+	}
+	.card1-box .cards-box .card-item{
+		width: 100%;
+		height: 150upx;
+		padding: 0 40upx;
+		box-sizing: border-box;
+	}
+	.card1-box .cards-box .card-item .name{
+		font-size: 34upx;
+		line-height: 150upx;
+		float: left;
+	}
+	.card1-box .cards-box .card-item image{
+		width: 100upx;
+		height: 100upx;
+		margin-top: 25upx;
+		margin-right: 30upx;
+		float: right;
+	}
+	#mask1{
+		position: fixed;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		background-color: rgba(0,0,0,0.5);
+	}
+	
+	page{
+		background-color: #f2f2f2;
+	}
+	.top{
+		width: 100%;
+		height: 400upx;
+		padding: 0 30upx;
+		box-sizing: border-box;
+	}
+	.top .card1{
+		width: 100%;
+		height: 100upx;
+		line-height: 100upx;
+		border-bottom: 1px solid #ccc;
+		box-sizing: border-box;
+	}
+	.top .card1:last-child{
+		border-bottom: none;
+	}
+	.top .card1 .name{
+		display: inline-block;
+		float: left;
+		line-height: 100upx;
+		width: 15%;
+	}
+	.top .card1 .card1Name{
+		width: 70%;
+		height: 100%;
+		display: inline-block;
+	}
+	.top .card1 .card1Name .left{
+		display: inline-block;
+		line-height: 100upx;
+		float: left;
+	}
+	.top .card1 .card1Name image{
+		display: inline-block;
+		margin-top: 20upx;
+		width: 60upx;
+		height: 60upx;
+		float: right;
+	}
+	.top .card1 .right-icon{
+		display: inline-block;
+		float: right;
+		width: 40upx;
+		margin-top: 25upx;
+		height: 50upx;
+	}
+	.top .card1 .right-icon image{
+		width: 100%;
+		height: 100%;
+	}
+	
+	.middle{
+		width: 100%;
+		height: 100upx;
+		display: flex;
+		justify-content: space-between;
+		background-color: #fff;
+		padding: 20upx 40upx;
+		box-sizing: border-box;
+		margin-top: 30upx;
+		margin-bottom: 30upx;
+	}
+	.middle>view{
+		display: inline-block;
+		width: 33.3%;
+		height: 100%;
+		line-height: 60upx;
+		text-align: center;
+	}
+	.middle .step{
+		border-left: 1px solid #ccc;
+		border-right: 1px solid #ccc;
+	}
+	
+	.bottom{
+		width: 100%;
+		height: 600upx;
+		padding: 30upx 40upx;
+		box-sizing: border-box;
+		background-color: #fff;
+	}
+	.bottom .title{
+		color: #d7362f;
+		font-size: 30upx;
+		height: 40upx;
+		line-height: 40upx;
+		padding-left: 10upx;
+		border-left: 4px solid #d7362f;
+	}
+</style>
